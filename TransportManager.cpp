b8e7a4b9defbe3e03b38dbd3852e7c1c21e12666
mykolamateichuk <mateichukmykola@gmail.com>
2022-10-26 19:42:55 +0300

    Added inputData function

--- TransportManager.cpp
+++ TransportManager.cpp
@@ -3,12 +3,6 @@
 TransportManager::TransportManager() {
     this->numberOfVehicles = 0;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            Map[i][j] = 0;
-        }
-    }
-
     /*
         ...
     */
@@ -23,4 +17,20 @@ TransportManager::~TransportManager() {
     /*
         ...
     */
+}
+
+void TransportManager::inputData() {
+    cout << "Number of vehicles to simulate: ";
+    cin >> numberOfVehicles;
+
+    cout << "Number of points on the map: ";
+    cin >> numberOfPoints;
+
+    cout << "Weighted graph representation of the map:" << endl;
+    for (int i = 0; i < numberOfPoints; i++) {
+        for (int j = 0; j < numberOfPoints; j++) {
+            cout << "Map[" << i << "][" << j << "]: ";
+            cin >> Map[i][j];
+        }
+    }
 }
--- TransportManager.h
+++ TransportManager.h
@@ -1,6 +1,8 @@
 #ifndef TRANSPORT_MANAGER
 #define TRANSPORT_MANAGER
 
+#include "Vehicle.h"
+
 class TransportManager {
     private:
         /*
@@ -8,22 +10,23 @@ class TransportManager {
         */
     public:
         unsigned int numberOfVehicles;
-        // Vehicle* allVehicles;
+        Vehicle* allVehicles;
 
         unsigned int **Map;
         // Road* roads;
+        unsigned int numberOfPoints;
         // Point* points;
 
         TransportManager();
 
-        /*
+        
         void inputData();
         void setUpSimulation();
         void addVehicle();
         void editMap();
         void printStats();
         void simulate();
-        */
+        
 
         ~TransportManager();
 };
