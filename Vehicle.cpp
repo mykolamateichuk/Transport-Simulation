56287b06a2fe23300e05d84db5062be73b53a9e5
Krendelbki <114921587+Krendelbki@users.noreply.github.com>
2022-11-14 21:43:30 +0300

    Add files via upload

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -8,7 +8,7 @@ void Vehicle::update_position()
 {
     removal += speed;
 
-    if (removal >= 1000) // !
+    if (removal >= 1000)
     {
         removal = 0;
         position = route.front();
@@ -40,12 +40,19 @@ void Vehicle::expandRoute(vector<int> poins)
 
 Vehicle::Vehicle(string number, int speed, int gasCapacity, int gasLevel, int position, Navigator *navigator)
 {
-    this->position = position;
+    this->removal = 0;
+    this->canMove = true;
     this->number = number;
+    this->position = position;
     this->navigator = navigator;
 
     if (speed < 0 || gasCapacity < 0 || gasLevel < 0 || gasCapacity < gasLevel)
+    {
+        this->speed = 40;
+        this->gasCapacity = 100;
+        this->gasLevel = this->gasCapacity;
         return;
+    }
 
     this->speed = speed;
     this->gasCapacity = gasCapacity;
@@ -97,3 +104,28 @@ void Vehicle::update()
     update_gasLevel();
     update_position();
 }
+
+// get
+int Vehicle::getPosition() { return position; }
+
+int Vehicle::getAppointment()
+{
+    if (appointments.empty())
+        return -1;
+
+    return appointments.front();
+}
+
+queue<int> Vehicle::getRoute() { return route; }
+
+int Vehicle::getSpeed() { return speed; }
+string Vehicle::getNumber() { return number; }
+
+int Vehicle::getGasLevel() { return gasLevel; }
+int Vehicle::getGasCapacity() { return gasCapacity; }
+
+// set
+void Vehicle::setNumber(string number) { this->number = number; }
+void Vehicle::setSpeed(int speed) { this->speed = speed >= 0 ? speed : 0; }
+void Vehicle::setGasLevel(int gasLevel) { this->gasLevel = gasLevel <= gasCapacity ? gasLevel : gasCapacity; }
+void Vehicle::setGasCapacity(int gasCapacity) { this->gasCapacity = gasCapacity > gasLevel ? gasCapacity : gasLevel; }
