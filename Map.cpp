386a0e2b8eb1118fd992fc17d8f2bdc39146501e
Mateichuk Mykola <mateichukmykola@gmail.com>
2023-01-13 19:42:21 +0200

    Add size field to Map class

--- Map.cpp
+++ Map.cpp
@@ -1,23 +1,30 @@
 #include "Map.h"
 
-Map::Map() {
+Map::Map() : size(0) {
 	map.resize(0);
 }
 
 Map::Map(std::vector<std::vector<unsigned int>> _map) 
-	: map(_map)
+	: map(_map),
+	  size(_map.size())
 {}
 
 Map::Map(Map& _mapClass) 
-	: map(_mapClass.map)
+	: map(_mapClass.map),
+	  size(_mapClass.map.size())
 {}
 
 std::vector<std::vector<unsigned int>> Map::getMap() const {
 	return map;
 }
 
+unsigned int Map::getSize() const {
+	return size;
+}
+
 Map& Map::setMap(std::vector<std::vector<unsigned int>> _map) {
 	map = _map;
+	size = _map.size();
 	return *this;
 }
 
--- Map.h
+++ Map.h
@@ -4,13 +4,16 @@
 #include <vector>
 
 class Map {
-private: 
+private:
+	unsigned int size;
+
 	std::vector<std::vector<unsigned int>> map;
 public:
 	Map();
 	Map(std::vector<std::vector<unsigned int>>);
 	Map(Map&);
 
+	unsigned int getSize() const;
 	std::vector<std::vector<unsigned int>> getMap() const;
 
 	Map& setMap(std::vector<std::vector<unsigned int>>);
