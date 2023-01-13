8d2bf7cbe7fef2cd87abc01499e4b4bd66495e01
Mateichuk Mykola <mateichukmykola@gmail.com>
2023-01-13 20:53:31 +0200

    Upgrade class Cell with new functionality

--- Cell.cpp
+++ Cell.cpp
@@ -2,25 +2,32 @@
 
 Cell::Cell() 
 	: point(NoType),
-	  roadLength(0)
+	  numberOfRoads(0),
+	  roadLengths(0)
 {}
 
-Cell::Cell(PointType _point, unsigned int _roadLength)
+Cell::Cell(PointType _point, unsigned int _numberOfRoads, const std::vector<unsigned int>& _roadLengths)
 	: point(_point),
-	  roadLength(_roadLength)
+	  numberOfRoads(_numberOfRoads),
+	  roadLengths(_roadLengths)
 {}
 
 Cell::Cell(const Cell& _cell) 
 	: point(_cell.point),
-	  roadLength(_cell.roadLength)
+	  numberOfRoads(_cell.numberOfRoads),
+	  roadLengths(_cell.roadLengths)
 {}
 
 PointType Cell::getPoint() const {
 	return point;
 }
 
-unsigned int Cell::getRoadLength() const {
-	return roadLength;
+unsigned int Cell::getNumberOfRoads() const {
+	return numberOfRoads;
+}
+
+std::vector<unsigned int> Cell::getRoadLengths() const {
+	return roadLengths;
 }
 
 Cell& Cell::setPoint(PointType _point) {
@@ -28,7 +35,12 @@ Cell& Cell::setPoint(PointType _point) {
 	return *this;
 }
 
-Cell& Cell::setRoadLength(unsigned int _roadLength) {
-	roadLength = _roadLength;
+Cell& Cell::setNumberOfRoads(unsigned int _numberOfRoads) {
+	numberOfRoads = _numberOfRoads;
+	return *this;
+}
+
+Cell& Cell::setRoadLengths(const std::vector<unsigned int>& _roadLengths) {
+	roadLengths = _roadLengths;
 	return *this;
 }
--- Map.h
+++ Map.h
@@ -3,21 +3,25 @@
 
 #include <vector>
 #include "Point.h"
+#include "Road.h"
 
 class Cell {
 private:
 	PointType point;
-	unsigned int roadLength;
+	unsigned int numberOfRoads;
+	std::vector<unsigned int> roadLengths;
 public:
 	Cell();
-	Cell(PointType, unsigned int);
+	Cell(PointType, unsigned int, const std::vector<unsigned int>&);
 	Cell(const Cell&);
 
 	PointType getPoint() const;
-	unsigned int getRoadLength() const;
+	unsigned int getNumberOfRoads() const;
+	std::vector<unsigned int> getRoadLengths() const;
 
 	Cell& setPoint(PointType);
-	Cell& setRoadLength(unsigned int);
+	Cell& setNumberOfRoads(unsigned int);
+	Cell& setRoadLengths(const std::vector<unsigned int>&);
 };
 
 class Map {
